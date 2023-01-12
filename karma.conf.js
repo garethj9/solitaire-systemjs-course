9d8890211644129e2910eaa819938839715d3727
garethj9 <117467018+garethj9@users.noreply.github.com>
2023-01-12 17:54:35 +0000

    Update karma.conf.js
    Remove Safari launcher from plugins

--- karma.conf.js
+++ karma.conf.js
@@ -34,8 +34,7 @@ module.exports = function (config) {
       "karma-firefox-launcher",
       "karma-jasmine",
       "karma-junit-reporter",
-      "karma-phantomjs-launcher",
-      "karma-safari-launcher"
+      "karma-phantomjs-launcher"      
     ],
 
     junitReporter: {
